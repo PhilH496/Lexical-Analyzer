962e759d68d1c050398665c9b3d2ee93a4d2df68
Phil <phiillyh@gmail.com>
2025-04-10 18:53:00 -0700

    Add files via upload

---
+++ DFA.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include "DFA.h"
+
+//---------------------------------------------------------------------------------
+// DFA ctor
+//---------------------------------------------------------------------------------
+DFA::DFA(set<char> A, set<int> I, set<int> F) : init_states(I), fin_states(F)
+{
+    Reset();
+}
+
+void DFA::Reset() 
+{
+    status = START; 
+    current_state = *init_states.begin(); 
+    
+    // Check if the initial state is a final state
+    if (fin_states.count(current_state) > 0) {
+        status = ACCEPT;
+        accepted = true;
+        lexeme.clear();
+        accepted_lexeme = lexeme;
+    } else {
+        accepted = false;
+    }
+    
+    lexeme.clear();
+}
+
+//---------------------------------------------------------------------------------
+// Move from one state, s, to another based on input char,
+// possibly changing both the current state and the status of the DFS
+//---------------------------------------------------------------------------------
+void DFA::Move( char c) 
+{ 
+    //---- check if c is in the map for transitions
+    if (Dtran[current_state].find(c) != Dtran[current_state].end()) 
+    {
+        current_state = Dtran[current_state][c];
+        lexeme += c;
+
+        if (fin_states.count(current_state) > 0)
+        {
+            status = ACCEPT;
+            accepted = true;
+            accepted_lexeme = lexeme;
+        }
+        else
+            status = POTENTIAL;
+    }
+    else
+        status = FAIL;
+}
+
+//---------------------------------------------------------------------------------
+// print the DFA
+//---------------------------------------------------------------------------------
+void DFA::Print() 
+{
+    cout << "DFA Transitions:\n";
+    for (const auto& dfa_row : Dtran) 
+    {
+        cout << dfa_row.first << ":\t";
+        for (const auto& transition : dfa_row.second) 
+            cout << transition.first << ": " << transition.second << " "; 
+        cout << endl;
+    }
+}
+
+//---------------------------------------------------------------------------------
+// check if the DFA's initial state is in the set of final states
+//---------------------------------------------------------------------------------
+bool DFA::acceptsEmptyString() const {
+    bool accepts = fin_states.count(*init_states.begin()) > 0;
+    return accepts;
+}
---
+++ DFA.h
@@ -0,0 +1,42 @@
+#ifndef DFA_H
+#define DFA_H
+
+#include <set>
+#include <map>
+
+using namespace std;
+
+enum DFAstatus {START, FAIL, POTENTIAL, ACCEPT};
+
+//---------------------------------------------------------------------------------
+// class DFA
+// (S, Σ, δ, s0, F)
+//---------------------------------------------------------------------------------
+class DFA 
+{
+public:
+    DFA() {}
+    DFA(set<char> A, set<int> I, set<int> F);
+    void Reset();
+    void AddTransition( int src, int dst, char sym) {Dtran[src][sym] = dst;}
+    void setFinalStates ( const set<int> &newFinalStates) { fin_states = newFinalStates; }
+    void Move( char c);
+    DFAstatus GetStatus() {return status;}
+    string GetAcceptedLexeme() {return accepted_lexeme;}
+    bool GetAccepted() {return accepted;}
+    void Print();
+    bool IsDead() const { return status == FAIL; }
+    bool acceptsEmptyString() const;
+
+private:
+    map< int, map<char, int> > Dtran;
+    DFAstatus status;
+    int current_state;
+    bool accepted;
+    string lexeme;
+    string accepted_lexeme;
+    set<int> init_states; // initial state of the DFA
+    set<int> fin_states;  // final states of the DFA
+};
+
+#endif
